966022df98005a93218d4881e0db5efa5ca5c287
Lorenzo Gaifas <brisvag@gmail.com>
2024-07-26 17:55:23 +0200

    new images, some todos done

--- scripts/cc_image.py
+++ scripts/cc_image.py
@@ -24,7 +24,7 @@ df = pd.DataFrame(
     {
         'spike': spike,
         'blob': blob,
-        'cross correlation': cc,
+        'cross-correlation': cc,
         'empty': np.full(N, np.nan),  # just so we get an extra subplot
     },
     index=x,
@@ -32,18 +32,30 @@ df = pd.DataFrame(
 df.columns.name = 'function'
 df.index.name = 'x'
 
-fig = px.line(df, facet_row='function', facet_row_spacing=0.01)
+fig = px.line(df, facet_row='function', facet_row_spacing=0.03)
 
 # add spike and shifted blob to bottom subplot
 blob_shifted = np.roll(df.blob, np.argmax(cc) - N // 2)
-fig.add_trace(go.Scatter(x=df.index, y=spike, line_color=px.colors.qualitative.Plotly[0]), row=1, col=1)
-fig.add_trace(go.Scatter(x=df.index, y=blob_shifted, line_color=px.colors.qualitative.Plotly[1]), row=1, col=1)
+colors = px.colors.qualitative.Plotly
+fig.add_trace(go.Scatter(x=df.index, y=spike, line_color=colors[0]), row=1, col=1)
+fig.add_trace(go.Scatter(x=df.index, y=blob_shifted, line_color=colors[1]), row=1, col=1)
+fig.add_trace(go.Scatter(x=df.index, y=spike, line_color=colors[0]), row=1, col=1)
+
+# lines for cc
+fig.add_vline(x=2 * np.pi * (np.argmax(cc) / N), line_width=3, line_dash="dash", line_color=colors[4], row=2, col=1)
+fig.add_vline(x=np.pi, line_width=3, line_dash="dash", line_color=colors[4], row=2, col=1)
+
+# lines for blob
+fig.add_vline(x=2 * np.pi * (np.argmax(blob) / N), line_width=3, line_dash="dash", line_color=colors[5], row=1, col=1)
+fig.add_vline(x=2 * np.pi * (np.argmax(blob_shifted) / N), line_width=3, line_dash="dash", line_color=colors[5], row=1, col=1)
+
 # hide legend for everything in last subplot
 fig.update_traces({'showlegend': False}, row=1, col=1)
 
 # clean up useless details and layout
 fig.update_xaxes(visible=False, fixedrange=True)
 fig.update_yaxes(visible=False, fixedrange=True)
-fig.update_layout(annotations=[], overwrite=True, margin=dict(l=20, r=20, t=20, b=20), plot_bgcolor='white', paper_bgcolor='white')
+fig.update_layout(annotations=[], overwrite=True, margin=dict(l=20, r=20, t=20, b=20), plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
 
-fig.write_image(Path(__file__).parent.parent / "img/introduction/cross_correlation.png", width=800, height=400, scale=3)
+fig.write_image(Path(__file__).parent.parent / "img/introduction/cross_correlation.png", width=800, height=300, scale=3)
+fig.show()
---
+++ scripts/filter.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import numpy as np
+from PIL import Image
+from scipy.fft import fftn, fftshift, ifftn, ifftshift
+import napari
+
+def smoothstep_normalized(arr, min_val, max_val):
+    rng = max_val - min_val
+    normalized = (arr - min_val) / rng
+    smooth = np.where(
+        normalized < 0,
+        0,
+        np.where(normalized <= 1, 3 * normalized**2 - 2 * normalized**3, 1),
+    )
+    return 1 - smooth
+
+
+def annulus(shape, start, end, padding=0.0):
+    center = shape[0] / 2
+    ranges = [np.arange(0, d) for d in shape]
+    indices = np.stack(np.meshgrid(*ranges, indexing="ij"), axis=-1) + 0.5
+    dists = np.linalg.norm(indices - center, axis=-1)
+
+    radius = end * shape[0] / 2
+    inner_radius = start * shape[0] / 2
+    padding = padding * shape[0] / 2
+    mask = smoothstep_normalized(dists, radius, radius + padding)
+    if inner_radius:
+        inner_mask = smoothstep_normalized(dists, inner_radius, inner_radius + padding)
+        mask -= inner_mask
+    return mask
+
+
+
+if __name__ == '__main__':
+    im = Image.open('/home/brisvag/Downloads/moyai.jpg').convert('L')
+    a = np.asarray(im)
+    ft = fftshift(fftn(a))
+    ps = np.log(np.abs(ft)**2 - 1)
+
+    lowpass = annulus(a.shape, 0, .05, padding=0.01)
+    bandpass = annulus(a.shape, .02, .1, padding=0.01)
+    highpass = annulus(a.shape, .1, 2, padding=0.01)
+
+    ft_low = ft * lowpass
+    ft_band = ft * bandpass
+    ft_high = ft * highpass
+
+    ps_low = np.log(np.abs(ft_low)**2 - 1)
+    ps_band = np.log(np.abs(ft_band)**2 - 1)
+    ps_high = np.log(np.abs(ft_high)**2 - 1)
+
+    low = ifftn(ifftshift(ft_low)).real
+    band = ifftn(ifftshift(ft_band)).real
+    high = ifftn(ifftshift(ft_high)).real
+
+    v = napari.Viewer()
+    v.add_image(a)
+    v.add_image(ps)
+    v.add_image(low)
+    v.add_image(ps_low)
+    v.add_image(band)
+    v.add_image(ps_band)
+    v.add_image(high)
+    v.add_image(ps_high)
+
+    v.grid.enabled = True
+    v.grid.stride = -1
+    v.grid.shape = (-1, 4)
+
+    v.export_figure(Path(__file__).parent.parent / 'img/introduction/filtering.png')
+    # needs modification...
